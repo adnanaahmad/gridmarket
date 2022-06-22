f66897721f4ed98cd1bbb738938a27a9c6de4d25
Adnan Ahmad <adnanahmad.khayal@gmail.com>
2022-06-23 00:48:29 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -39,10 +39,10 @@ function App() {
         <Violinplot/>
       </div>
       <div style={{direction: 'row', display: 'flex', width: '100%', marginBottom: '100px'}}>
-        <div style={{margin: 'auto'}}>Country</div>
+        <div style={{margin: 'auto'}}>Hour</div>
         <div style={{width: '100%'}}>
           <HeatmapExample/>
-          <div style={{marginTop: 2, margin: 'auto', textAlign: 'center'}}>Longitude</div>
+          <div style={{marginTop: 2, margin: 'auto', textAlign: 'center'}}>Day</div>
         </div>
       </div>
       <div style={{marginBottom: 100, marginLeft: 50, marginTop: 20, fontWeight: 500, fontSize: 'large', textDecoration: 'underline'}}>Mock Data Examples</div>
--- src/components/heatmap.js
+++ src/components/heatmap.js
@@ -1,38 +1,48 @@
 import React, { useState, useEffect } from 'react';
 import { Heatmap } from '@ant-design/plots';
 import { data as realData } from '../data/testData';
+import hdata from '../visx/data/individual.json';
 
 export default function HeatmapExample() {
   const [data, setData] = useState([]);
 
   useEffect(() => {
-    let heatmapData = realData['Analyses'].map((node) => {
-        return {
-            Longitude: Number(node.Longitude), 
-            Country: node.Country,
-            'Capex: Payback Period': Number(node['Capex: Payback Period'])
-        }
+    // let heatmapData = realData['Analyses'].map((node) => {
+    //     return {
+    //         Longitude: Number(node.Longitude), 
+    //         Country: node.Country,
+    //         'Capex: Payback Period': Number(node['Capex: Payback Period'])
+    //     }
+    // });
+    let heatmapData = hdata.map((node) => {
+      return {
+          'hour': node.hour, 
+          'day': node.day,
+          'original_load': Number(node['original_load'])
+      }
     });
-
     setData(heatmapData);
 
-    setTimeout(()=> {
-      setData(heatmapData.slice(0,100));
-    }, 5000);
+    // setTimeout(()=> {
+    //   setData(heatmapData.slice(0,100));
+    // }, 5000);
 
   }, []);
 
   const config = {
     width: 650,
     height: 500,
-    autoFit: false,
+    autoFit: true,
     data,
-    xField: 'Longitude',
-    yField: 'Country',
-    colorField: 'Capex: Payback Period',
+    xField: 'day',
+    yField: 'hour',
+    colorField: 'original_load',
     color: ['#174c83', '#7eb6d4', '#efefeb', '#efa759', '#9b4d16'],
     meta: {
-      'Longitude': {
+      'day': {
+        type: 'cat',
+      },
+      'hour': {
         type: 'cat',
       },
     },
