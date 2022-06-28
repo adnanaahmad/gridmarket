460bfd5c9961261a366ca4fff9878c3863ec22d2
Adnan Ahmad <adnanahmad.khayal@gmail.com>
2022-06-28 17:31:27 +0500

    stackbar

--- src/components/stacked.js
+++ src/components/stacked.js
@@ -1,26 +1,49 @@
 import React, { useState, useEffect } from 'react';
 import { Column } from '@ant-design/plots';
 import barchartData from '../visx/data/stacked_chart.json';
+import { each, groupBy } from '@antv/util';
 
 export default function StackedBar () {
   const [data, setData] = useState([]);
+  const [annotations, setAnnotations] = useState([]); 
 
   useEffect(() => {
     let bdata =[];
+    let units = {
+      'CapEx': '($)',
+      'Savings': '($)',
+      'Generation': '(kW)',
+      'Demand Reduction': '(kW-month)',
+      'CO2': '(kg)'
+    }
 
     barchartData.forEach(x => {
+
       bdata.push({
-        'Type': x.Type,
-        'pct': Number(x.pct),
+        'Type': x.Type+units[x.Type],
+        'pct': x.Spending,
         'Group': x.Group
       })
     });
-    let b = [...bdata];
-    setData(b.slice(1, bdata.length/2));
+    setData(bdata);
+    const an = [];
+    each(groupBy(bdata, 'Type'), (values, k) => {
+      const value = values.reduce((a, b) => a + b.pct, 0);
+      an.push({
+        type: 'text',
+        position: [k, value],
+        content: `${value}`,
+        style: { textAlign: 'center', fontSize: 14, fill: 'rgba(0,0,0,0.85)' },
+        offsetY: -10,
+      });
+    });
+    setAnnotations(an);
+    // let b = [...bdata];
+    // setData(b.slice(1, bdata.length/2));
 
-    setTimeout(()=> {
-      setData(bdata);
-    }, 5000);
+    // setTimeout(()=> {
+    //   setData(bdata);
+    // }, 5000);
 
   }, []);
 
@@ -45,6 +68,7 @@ export default function StackedBar () {
         },
       ],
     },
+    annotations
   };
 
   return <Column {...config} />;
