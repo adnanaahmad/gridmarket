f49b86b3f6c54146b06172878373234b0f0be772
Adnan Ahmad <adnanahmad.khayal@gmail.com>
2022-06-28 16:16:52 +0500

    heatmap

--- src/components/heatmap.js
+++ src/components/heatmap.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import { Heatmap } from '@ant-design/plots';
-import { data as realData } from '../data/testData';
 import hdata from '../visx/data/individual.json';
 
 export default function HeatmapExample() {
@@ -12,15 +11,16 @@ export default function HeatmapExample() {
       return {
           'hour': node.hour, 
           'day': node.day,
-          'original_load': Number(node['original_load'])
+          'solar_gen': Number(node['solar_gen'])
       }
     });
-    let h = [...heatmapData];
-    setData(h.slice(1,274));
+    // let h = [...heatmapData];
+    // setData(h.slice(1,274));
 
-    setTimeout(()=> {
-      setData(heatmapData);
-    }, 5000);
+    // setTimeout(()=> {
+    //   setData(heatmapData);
+    // }, 5000);
+    setData(heatmapData);
 
   }, []);
 
@@ -31,8 +31,15 @@ export default function HeatmapExample() {
     data,
     xField: 'day',
     yField: 'hour',
-    colorField: 'original_load',
+    colorField: 'solar_gen',
     color: ['#174c83', '#7eb6d4', '#efefeb', '#efa759', '#9b4d16'],
+    legend: {
+      layout: 'vertical',
+      position: 'right',
+      title: {
+        text: 'solar_gen'
+      }
+    },
     meta: {
       'day': {
         type: 'cat',
@@ -43,5 +50,10 @@ export default function HeatmapExample() {
     },
   };
 
-  return <Heatmap {...config} />;
+  return (
+    <div>
+      <div style={{ fontWeight: 500, fontSize: 'large', textAlign: 'center', marginBottom: 10}}>Solar Generation</div>
+      <Heatmap {...config} />
+    </div>
+  );
 };
