26e73ffe8a534f946f67ec83e699a2a0f1118c67
Adnan Ahmad <adnanahmad.khayal@gmail.com>
2022-07-08 17:07:56 +0500

    heatmap in progres

--- src/components/heatmap.js
+++ src/components/heatmap.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Heatmap } from '@ant-design/plots';
 import hdata from '../visx/data/individual.json';
+import { Box, Stack, Typography } from '@mui/material';
 
 export default function HeatmapExample() {
   const [data, setData] = useState([]);
@@ -48,6 +49,28 @@ export default function HeatmapExample() {
         type: 'cat',
       },
     },
+    tooltip:{
+      customContent: (title, items) => {
+        const data = items[0]?.data || {};
+        const color = items[0]?.color || '#174c83';
+        let val = data.solar_gen ?  data.solar_gen.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") : data.solar_gen
+        console.log(data, items);
+        return (
+          <Box sx={{paddingX: 1, paddingY: 1.5}}>
+            <Stack spacing={2}>
+              <div>{data.day}</div>
+              <Stack direction={'row'} spacing={1} alignItems='center'>
+                <div style={{background: color, height: 10, width: 10, borderRadius: 10}}></div>
+                <div>solar_gen:</div>
+                <div>{val} kWh</div>
+              </Stack>
+            </Stack>
+          </Box>
+          )
+        // {hour: 14, day: 332, solar_gen: 31629}
+
+      }
+    }
   };
 
   return (
